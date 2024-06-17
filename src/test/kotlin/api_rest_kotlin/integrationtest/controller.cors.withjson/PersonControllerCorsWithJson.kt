268d45be989bb12e8bf4e96b0be4d3fca4c79321
Rafael Daitx <rafa.daitx.silva@rede.ulbra.br>
2024-06-17 17:28:17 -0300

    Unit Tests

--- src/test/kotlin/api_rest_kotlin/integrationtest/controller.cors.withjson/PersonControllerCorsWithJson.kt
+++ src/test/kotlin/api_rest_kotlin/integrationtest/controller.cors.withjson/PersonControllerCorsWithJson.kt
@@ -39,7 +39,7 @@ class PersonControllerCorsWithJson : AbstractIntegrationTest() {
         specification = RequestSpecBuilder()
             .addHeader(
                 TestConfigs.HEADER_PARAM_ORIGIN,
-                TestConfigs.ORIGIN_RAFAEL
+                TestConfigs.ORIGIN_LOCALHOST
                 )
             .setBasePath("/api/person/v1")
             .setPort(TestConfigs.SERVER_PORT)
@@ -52,6 +52,82 @@ class PersonControllerCorsWithJson : AbstractIntegrationTest() {
             .contentType(TestConfigs.CONTENT_TYPE_JSON)
             .body(person)
             .post()
+            .then()
+            .statusCode(200)
+            .extract()
+            .body()
+            .asString()
+
+        val createdPerson = objectMapper.readValue(
+            content,
+            PersonVO::class.java
+        )
+        person = createdPerson
+
+        assertNotNull(createdPerson.id)
+        assertNotNull(createdPerson.firstName)
+        assertNotNull(createdPerson.lastName)
+        assertNotNull(createdPerson.address)
+        assertNotNull(createdPerson.gender)
+        assertTrue(createdPerson.id > 0)
+
+        assertEquals("Nelson", createdPerson.firstName)
+        assertEquals("Piquet", createdPerson.lastName)
+        assertEquals("Brasilia", createdPerson.address)
+        assertEquals("Male", createdPerson.gender)
+    }
+
+    @Test
+    @Order(2)
+    fun testCreateWithWrongOrigin() {
+        mockPerson()
+
+        specification = RequestSpecBuilder()
+            .addHeader(
+                TestConfigs.HEADER_PARAM_ORIGIN,
+                TestConfigs.ORIGIN_RAFAEL
+            )
+            .setBasePath("/api/person/v1")
+            .setPort(TestConfigs.SERVER_PORT)
+            .addFilter(RequestLoggingFilter(LogDetail.ALL))
+            .addFilter(ResponseLoggingFilter(LogDetail.ALL))
+            .build()
+
+        val content = given()
+            .spec(specification)
+            .contentType(TestConfigs.CONTENT_TYPE_JSON)
+            .body(person)
+            .post()
+            .then()
+            .statusCode(403)
+            .extract()
+            .body()
+            .asString()
+
+        assertEquals("Invalid CORS request", content)
+    }
+
+    @Test
+    @Order(3)
+    fun testFindById() {
+        mockPerson()
+
+        specification = RequestSpecBuilder()
+            .addHeader(
+                TestConfigs.HEADER_PARAM_ORIGIN,
+                TestConfigs.ORIGIN_LOCALHOST
+            )
+            .setBasePath("/api/person/v1")
+            .setPort(TestConfigs.SERVER_PORT)
+            .addFilter(RequestLoggingFilter(LogDetail.ALL))
+            .addFilter(ResponseLoggingFilter(LogDetail.ALL))
+            .build()
+
+        val content = given()
+            .spec(specification)
+            .contentType(TestConfigs.CONTENT_TYPE_JSON)
+            .pathParam("id", person.id)
+            .`when`()["{id}"]
             .then()
             .statusCode(200)
             .extract()
@@ -75,6 +151,37 @@ class PersonControllerCorsWithJson : AbstractIntegrationTest() {
         assertEquals("Brasilia", createdPerson.address)
         assertEquals("Male", createdPerson.gender)
     }
+
+    @Test
+    @Order(4)
+    fun testFindByIdWithWrongOrigin() {
+        mockPerson()
+
+        specification = RequestSpecBuilder()
+            .addHeader(
+                TestConfigs.HEADER_PARAM_ORIGIN,
+                TestConfigs.ORIGIN_RAFAEL
+            )
+            .setBasePath("/api/person/v1")
+            .setPort(TestConfigs.SERVER_PORT)
+            .addFilter(RequestLoggingFilter(LogDetail.ALL))
+            .addFilter(ResponseLoggingFilter(LogDetail.ALL))
+            .build()
+
+        val content = given()
+            .spec(specification)
+            .contentType(TestConfigs.CONTENT_TYPE_JSON)
+            .pathParam("id", person.id)
+            .`when`()["{id}"]
+            .then()
+            .statusCode(403) //Forbidden
+            .extract()
+            .body()
+            .asString()
+
+        assertEquals("Invalid CORS request", content)
+    }
+
 
     private fun mockPerson() {
         person.firstName = "Nelson"
